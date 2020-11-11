0a7a4f0fdb120f70d5bfa85a01908144e98c0a91
belenkovs <65164103+belenkovs@users.noreply.github.com>
2020-11-11 20:53:19 +0400

    infoPanel убрал лишнее

--- src/components/infoPanel/infoPanel.js
+++ src/components/infoPanel/infoPanel.js
@@ -1,21 +1,12 @@
 import React from "react";
-import MyContext from '../myContext/myContext';
 
 const InfoPanel =() => {
 
 	return  (
-		
-		<MyContext.Consumer>
-			{
-				(value) => {
-					return (
+
 						<div className="name">
 							myName is {value.name}, age {value.age}
 						</div>
-					)
-				}
-			}
-		</MyContext.Consumer>
 
 	)
 
